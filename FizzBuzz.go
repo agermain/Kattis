4525d6b153f33daedae3a5cff897e5db901d94d7
Alejandro Germain <alexpg258@gmail.com>
2018-09-27 22:30:44 +0100

    solution to FizzBuzz

---
+++ FizzBuzz.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a, b, c int
+	fmt.Scanln(&a, &b, &c)
+
+	for i := 1; i <= c; i++ {
+		if i%a == 0 && i%b == 0 {
+			fmt.Printf("FizzBuzz\n")
+			continue
+		}else if i%a == 0 {
+			fmt.Printf("Fizz\n")
+			continue
+		}else if i%b == 0 {
+			fmt.Printf("Buzz\n")
+			continue
+		}else{
+			fmt.Printf("%d\n", i)
+		}
+	}
+}
