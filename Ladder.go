513a8498315dca2a17233f2dad0699392021c8b5
Alejandro Germain <alexpg258@gmail.com>
2018-09-27 22:32:01 +0100

    solution to Ladder

---
+++ Ladder.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+func compute(h, v float64) int {
+	v = v * math.Pi/180
+	return int(math.Ceil(h/math.Sin(v)))
+}
+func main() {
+	var a, b float64
+	fmt.Scanln(&a, &b)
+	fmt.Print(compute(a, b))
+
+}
