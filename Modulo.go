b313d413d9de37d376026c76f7ca5e8610bd1817
Alejandro Germain <alexpg258@gmail.com>
2018-09-29 00:53:53 +0100

    solution to Modulo

---
+++ Modulo.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+)
+func contains(s []int, e int) bool {
+	for _, a := range s {
+		if a == e {
+			return true
+		}
+	}
+	return false
+}
+func main() {
+	var r []int
+	for i:=0; i<10; i++ {
+		var a int
+		fmt.Scanln(&a)
+		v := a%42
+		if !contains(r, v) {
+			r = append(r, v)
+		}
+		}
+	fmt.Println(len(r))
+}
