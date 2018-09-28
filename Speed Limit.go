d550813a95266be1c615f152ba0371f50356fcb7
Alejandro Germain <alexpg258@gmail.com>
2018-09-28 11:09:23 +0100

    solution to Speed Limit

---
+++ Speed Limit.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	for;; {
+		var a int
+		fmt.Scanln(&a)
+		if a == -1 {
+			break
+		}
+		var finalT, distance = 0, 0
+		for i:=0;i < a;i++{
+			var speed, time int
+			fmt.Scanf("%d %d", &speed, &time)
+			time -= finalT
+			finalT += time
+			distance += time * speed
+		}
+		fmt.Printf("%d miles\n", distance)
+	}
+}
