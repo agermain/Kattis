ce34b410c8fa36eb8930c4d0a1e45523065703b0
Alejandro Germain <alexpg258@gmail.com>
2018-09-28 11:09:06 +0100

    solution to Grass Seed Inc.

---
+++ Grass Seed Inc..go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+func main() {
+	var a, r float64
+	var b int
+	fmt.Scanln(&a)
+	fmt.Scanln(&b)
+	for i := 0; i < b; i++ {
+		var w, l float64
+		fmt.Scanln(&w, &l)
+		r = r + (w * l)
+	}
+	fmt.Print(r*a)
+}
