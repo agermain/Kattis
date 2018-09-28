bda2f3f71ea760c18a66b3a10d9b9057ea4df8c8
Alejandro Germain <alexpg258@gmail.com>
2018-09-29 00:54:08 +0100

    solution to trik

---
+++ trik.py
@@ -0,0 +1,25 @@
+ans = 1
+for i in input():
+    if i == 'A'
+        if ans == 1:
+            ans = 2
+        elif ans == 2:
+            ans = 1
+        else:
+            ans = 3
+    elif i == 'B':
+        if ans == 1:
+            ans = 1
+        elif ans == 2:
+            ans = 3
+        else:
+            ans = 2
+
+    else:
+        if ans == 1:
+            ans = 3
+        elif ans == 2:
+            ans = 2
+        else:
+            ans = 1
+print(ans)
