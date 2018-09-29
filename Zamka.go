94b6cde5e6859a59f6e56ff2ed28fb4e384afa15
Alejandro Germain <alexpg258@gmail.com>
2018-09-29 22:13:58 +0100

    solution to Zamka

---
+++ Zamka.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"strconv"
+	"fmt"
+	"math"
+)
+
+func main() {
+	var l, d, x, m, sum int
+	var n = math.MaxInt64
+	fmt.Scanln(&l)
+	fmt.Scanln(&d)
+	fmt.Scanln(&x)
+	for i:=l; i<=d; i++ {
+		s := strconv.Itoa(i)
+		for j:=0; j<len(s); j++ {
+			sum += int(s[j]-'0')
+		}
+		if sum == x {
+			if i < n {
+				n = i
+			}
+			if i > m {
+				m = i
+			}
+		}
+		sum = 0
+	}
+	fmt.Printf("%d\n%d", n, m)
+}
