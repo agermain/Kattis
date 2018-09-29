0aec3f7fbe5052b75e730e1e8f36d59229509a54
Alejandro Germain <alexpg258@gmail.com>
2018-09-29 16:34:41 +0100

    solution to FIlip

---
+++ Filip.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+func Reverse(s string) string {
+	n := len(s)
+	runes := make([]rune, n)
+	for _, rune := range s {
+		n--
+		runes[n] = rune
+	}
+	return string(runes[n:])
+}
+func Max(x, y int64) int64 {
+	if x < y {
+		return y
+	}
+	return x
+}
+func main() {
+	var a, b string
+	fmt.Scanln(&a, &b)
+	c, _ := strconv.ParseInt(Reverse(a), 10, 0 )
+	d, _ := strconv.ParseInt(Reverse(b), 10, 0 )
+	fmt.Printf("%d", Max(c, d))
+}
