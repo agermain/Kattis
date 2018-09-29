08448123a98d283a997fcab5a5c6030b1414e4d9
Alejandro Germain <alexpg258@gmail.com>
2018-09-29 09:55:06 +0100

    solution to Kornislav

---
+++ Kornislav.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+import "sort"
+
+func main() {
+		var a, b, c, d int
+		fmt.Scanln(&a, &b, &c, &d)
+		ints := []int{a, b, c, d}
+		sort.Ints(ints)
+		fmt.Printf("%d", ints[0]*ints[2])
+	}
