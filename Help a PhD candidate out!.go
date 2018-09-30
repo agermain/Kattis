2dec854e1aaaee5d5eb27464a39be14b0d17a7cc
Alejandro Germain <alexpg258@gmail.com>
2018-09-30 14:46:17 +0100

    solution to Help a PhD candidate out!

---
+++ Help a PhD candidate out!.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	var a int
+	fmt.Scanln(&a)
+	for i:=0; i<a; i++ {
+		var s string
+		fmt.Scanln(&s)
+		if s != "P=NP" {
+			sp := strings.Split(s, "+")
+			i1, _ := strconv.Atoi(sp[0])
+			i2, _ := strconv.Atoi(sp[1])
+			fmt.Println(i1+i2)
+		} else {
+			fmt.Printf("skipped\n")
+		}
+	}
+}
