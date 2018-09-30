5af2cd7ab9b336c46c75a91776e5bf0924ad9f75
Alejandro Germain <alexpg258@gmail.com>
2018-09-30 11:10:47 +0100

    solution to Autori

---
+++ Autori.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"unicode"
+)
+
+func main() {
+	var a, b string
+	fmt.Scanln(&a)
+	for _, char := range a {
+		if unicode.IsUpper(char){
+			b += string(char)
+		}
+	}
+	fmt.Println(b)
+}
