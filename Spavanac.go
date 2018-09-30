2cdf8076d9568b9549575622d35e3a4f70feaee1
Alejandro Germain <alexpg258@gmail.com>
2018-09-30 12:08:55 +0100

    solution to Spavanac

--- Spavanac.go
+++ Spavanac.go
@@ -1,1 +1,17 @@
 package main
+
+import "fmt"
+
+func main() {
+	var a, b int
+	fmt.Scanln(&a, &b)
+	b -= 45
+	if 0 > b {
+		b += 60
+		a--
+		if 0 > a {
+			a += 24
+		}
+	}
+	fmt.Printf("%d %d", a, b)
+}
