9a291a3655ac590e274f2d4b77f03177c5be72a4
Alejandro Germain <alexpg258@gmail.com>
2018-10-01 10:28:02 +0100

    solution to Synchronizing Lists

---
+++ Synchronizing Lists.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	var a int
+	fmt.Scanln(&a)
+	for {
+		m := make(map[int]int)
+		var n1, sl1, sl2 []int
+		for i:=0; i<a; i++ {
+			var d int
+			fmt.Scanln(&d)
+			n1 = append(n1, d)
+			sl1 = append(sl1, d)
+		}
+		for i:=0; i<a; i++ {
+			var d int
+			fmt.Scanln(&d)
+			sl2 = append(sl2, d)
+		}
+
+		sort.Ints(sl1)
+		sort.Ints(sl2)
+
+		for i:=0; i<a; i++ {
+			m[sl1[i]] = sl2[i]
+		}
+		for i:=0; i<a; i++ {
+			fmt.Printf("%d\n", m[n1[i]])
+		}
+		fmt.Scanln(&a)
+		if a == 0 {
+			break
+		}
+		fmt.Println()
+	}
+}
