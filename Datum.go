ab77b51106e9922f8db5b053f7537d9f66969324
Alejandro Germain <alexpg258@gmail.com>
2018-10-03 18:48:17 +0100

    solution to Datum

---
+++ Datum.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"time"
+	"fmt"
+)
+
+func main() {
+	var d int
+	var m time.Month
+	fmt.Scanf("%d %d", &d, &m)
+	t := time.Date(2009, m, d, 20, 34, 58, 651387237, time.UTC)
+	fmt.Println(t.Weekday())
+}
