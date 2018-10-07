04320cd539a6ad7f4e5b77d7832b69c2ee072fd8
Alejandro Germain <alexpg258@gmail.com>
2018-10-07 16:23:05 +0100

    solution to Heart Rate

---
+++ Heart Rate.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var n int
+	var b, p float64
+	fmt.Scanln(&n)
+	for i:=0; i<n; i++ {
+		fmt.Scanln(&b, &p)
+		fmt.Printf("%.4f %.4f %.4f\n", ((60/p)*b)-(60/p), (60/p)*b, ((60/p)*b)+(60/p))
+	}
+}
