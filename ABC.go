e6b5d794738518448c7119b27be3e13ff8896c10
Alejandro Germain <alexpg258@gmail.com>
2018-10-07 16:24:08 +0100

    solution to ABC

---
+++ ABC.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	var a, b, c int
+	var s string
+	fmt.Scanln(&a, &b, &c)
+	fmt.Scanln(&s)
+	var l = []int{a, b, c}
+	sort.Ints(l)
+	for i:=0; i<3; i++ {
+		if string(s[i]) == "A" {
+			fmt.Printf("%d ", l[0])
+		} else if string(s[i]) == "B" {
+			fmt.Printf("%d ", l[1])
+		} else {
+			fmt.Printf("%d ", l[2])
+		}
+	}
+}
