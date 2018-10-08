efe72eb63a7a70751990276025fd2a57ae3f319b
Alejandro Germain <alexpg258@gmail.com>
2018-10-08 11:39:48 +0100

    solution to Bus

---
+++ Bus.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	var t int
+	fmt.Scanln(&t)
+	for i:=0; i<t; i++ {
+		var k float64
+		fmt.Scanln(&k)
+		fmt.Println(int(math.Pow(2, k)-1))
+	}
+}
