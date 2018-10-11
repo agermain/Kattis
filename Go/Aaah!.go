d7e6f81c784330e139282297f4b8e38846ba03e6
Alejandro Germain <alexpg258@gmail.com>
2018-10-11 11:46:29 +0100

    solution to Aaah!

---
+++ Go/Aaah!.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+"bufio"
+"fmt"
+"os"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	l1 := len(scanner.Bytes())
+	scanner.Scan()
+	l2 := len(scanner.Bytes())
+	if l1 >= l2 {
+		fmt.Println("go")
+	} else {
+		fmt.Println("no")
+	}
+}
