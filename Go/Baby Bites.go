34afa92a2a76ab0b8ae6f97caf3880ee3868cc18
Alejandro Germain <alexpg258@gmail.com>
2018-10-13 11:51:09 +0100

    solution to Baby Bites

---
+++ Go/Baby Bites.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "os"
+    "strconv"
+    "strings"
+)
+
+func toInt(buf []byte) (n int) {
+    for _, v := range buf {
+        n = n*10 + int(v-'0')
+    }
+    return
+}
+
+
+func main() {
+    scanner := bufio.NewScanner(os.Stdin)
+    scanner.Scan()
+    var _ = toInt(scanner.Bytes())
+    scanner.Scan()
+    s := strings.Split(scanner.Text(), " ")
+    var b = true
+    for i, v := range s {
+        if strconv.Itoa(i+1) == v || v == "mumble"{
+            continue
+        }
+        b = false
+        break
+    }
+    if b == false {
+        fmt.Println("something is fishy")
+    } else {
+        fmt.Println("makes sense")
+    }
+}
