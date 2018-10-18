14689a3147474f4b9e3acdcecfad49e4d87c8baa
Alejandro Germain <alexpg258@gmail.com>
2018-10-18 21:34:45 +0100

    solution to Hidden Password

---
+++ Go/Hidden Password.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "os"
+    "strings"
+)
+func hidden(a, b string) string {
+    for i:=0; i<len(b); i++ {
+        if strings.Contains(a, string(b[i])) {
+            if b[i] != a[0] {
+                return "FAIL"
+            }
+            a = strings.Replace(a, string(b[i]), "", 1)
+        }
+    }
+    if len(a) == 0 {
+        return "PASS"
+    } else {
+        return "FAIL"
+    }
+}
+
+func main() {
+    scanner := bufio.NewScanner(os.Stdin)
+    scanner.Scan()
+    var a, b string
+    fmt.Sscanf(scanner.Text(), "%v %v", &a, &b)
+    fmt.Println(hidden(a, b))
+
+}
