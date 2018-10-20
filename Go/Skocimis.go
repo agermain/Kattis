1505a590bf7249a5c2f1954056742317b3e9d8d2
Alejandro Germain <alexpg258@gmail.com>
2018-10-20 19:49:47 +0100

    solution to Skocimis

---
+++ Go/Skocimis.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+func Max (a, b int64) int64{
+	if a > b {
+		return a
+	}
+	return b
+}
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	var a, b, c int64
+	fmt.Sscanf(scanner.Text(), "%d %d %d ", &a, &b, &c)
+	fmt.Println(Max(b-a, c-b)-1)
+}
